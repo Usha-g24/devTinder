9750bec1308a1bfccf711ead27e6b4539672b492
ushagiddaluri@gmail.com <ushagiddaluri@gmail.com>
2025-01-16 09:54:37 +0530

    CRUD&query and params exploring

--- app.js
+++ app.js
@@ -10,16 +10,16 @@ const app =express();
 // app.use("/E sarees",(req,res)=>{
 //     res.send("Lakshmi");
 // });
-app.get("/user",(req,res)=>{
-    res.send({"FirstName":"usha",
-        "LastName":"G"});
+app.get("/user/:userid/:password",(req,res)=>{
+    console.log(req.params);
+    res.send({"FirstNAme":"usha"});
 });
-app.post("/user",(req,res)=>{
-    res.send("post this matte succesfull");
-});
-app.put("/user",(req,res)=>{
-    res.send("matter wa sucessfully put in the database");
-});
+// app.post("/user",(req,res)=>{
+//     res.send("post this matte succesfull");
+// });
+// app.put("/user",(req,res)=>{
+//     res.send("matter wa sucessfully put in the database");
+// });
 // app.update("/user",(req,res)=>{
 //     res.send("update the challenge");
 // });
