654e335bac6b20d703144799f45b4aad86280df8
ushagiddaluri@gmail.com <ushagiddaluri@gmail.com>
2025-01-16 09:01:40 +0530

    Exploring get,post,put,delete

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const app =express();
-app.use('/text',(req,res)=>{
-    res.send("server is created");
+
+/// **order of the code is very important**///
+
+// app.use('/text',(req,res)=>{
+//     res.send("E-SAREES");
+// });
+
+// app.use("/E sarees",(req,res)=>{
+//     res.send("Lakshmi");
+// });
+app.get("/user",(req,res)=>{
+    res.send({"FirstName":"usha",
+        "LastName":"G"});
 });
-app.use('/hello',(req,res)=>{
-    res.send("helo helo");
+app.post("/user",(req,res)=>{
+    res.send("post this matte succesfull");
 });
-app.listen(7777,()=>{
-    console.log("sucessfully register");
+app.put("/user",(req,res)=>{
+    res.send("matter wa sucessfully put in the database");
+});
+// app.update("/user",(req,res)=>{
+//     res.send("update the challenge");
+// });
+
+app.listen(786,(req,res)=>{
+    console.log("E-Sarees");
 });
