4cf1b0f7eabdb9ddb820b148a504a33f6085dd9b
ushagiddaluri@gmail.com <ushagiddaluri@gmail.com>
2025-01-21 20:58:14 +0530

    schema validations

--- src/app.js
+++ src/app.js
@@ -4,12 +4,12 @@ const app =express();
 const User = require("./models/user");
  
 app.use(express.json());
-
+// new  user to signup
 app.post("/signup",async(req,res)=>{
     const user = new User(req.body);
     try{
         await user.save();
-        res.send("user is created successfully");
+        res.send("user Added successfully");
     }
     catch (err){
         res.status(500).send("error"+err.message);
@@ -17,6 +17,62 @@ app.post("/signup",async(req,res)=>{
     
 });
 
+// get all user from API
+app.get("/user",async(req,res)=>{
+    
+    const userEmail = req.body.email;
+   
+    try{
+        console.log("userEmail : "+userEmail);
+        const user = await  User.findOne({email:userEmail});
+        if(!user){
+            res.status(404).send("user found"); 
+        }
+        else{
+            res.send(user);
+        }
+    }
+    catch(err){
+        res.status(500).send("something went wrong");
+    }
+
+});
+
+ app.get("/feed",async(req,res)=>{
+    const userEmail = req.body.email;
+    try{
+    const users = await User.find({});
+    res.send(users);
+    }
+    catch(err){
+        res.status(500).send("error"+err.message);
+    }
+
+ });
+// delete from API
+ app.delete("/user",async(req,res)=>{
+    const  userId = req.body.userId; 
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        res.send("user delete successfully");
+    }
+    catch(err){
+        res.status(500).send("error"+err.message);
+    }
+ });
+// update from API
+app.patch("/user",async(req,res)=>{
+    const userId = req.body.userId;
+    const update = req.body;
+    console.log("update : "+update);
+    try{
+        const user = await User.findByIdAndUpdate(userId,update);
+        res.send('user update successfully');
+    }
+    catch(err){
+        res.status(500).send("error"+err.message);
+    }
+});
 // database connection
 connectDB()
     .then(()=>{
--- src/models/user.js
+++ src/models/user.js
@@ -1,21 +1,46 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    fristName:{
+    firstName:{
         type:String,
+        required:true,
+        // minLength:200,
+        // maxLength:150,
     },
     lastName:{
         type:String,
+        required:true,
     },
     gender:{
         type:String,
+        validate(value){
+            if (!["male","female","other"].includes(value)){
+                throw new Error("gender is not valid");}
+        }
+    },
+    photoUrl:{
+        type:String,
+        default:"https://www.shutterstock.com/image-illustration/success-usually-comrs-those-who-busy-2368394019",
+    },
+    about:{
+        type:String,
+        default:"Hey there! i am a developer.",
     },
     email:{
         type: String,
+        lowercase: true ,
+        trim:true,
     },
     password:{
-        type:String
+        type:String,
+        trim:true,
+        lowercase:true,
+        uppercase:true,
+        number:true,
     }
+},
+{
+    timestamps:true,
 });
 
 
